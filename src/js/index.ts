505647ed9fe1b4ab3b1eebabf1f59123f202b895
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-03-27 13:36:24 +0100

    Tilføjet addcar i typescript og html

--- src/js/index.ts
+++ src/js/index.ts
@@ -8,7 +8,35 @@ import { ICar } from "./Icar";
 let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 let GetAllCarsButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("getAllButton");
 
-GetAllCarsButton.addEventListener('click',showAllCars)
+GetAllCarsButton.addEventListener('click',showAllCars);
+
+let AddCarButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("addButton");
+
+AddCarButton.addEventListener('click',addCar);
+
+function addCar():void{
+    let addModelelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addModel");
+    let addVendorelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addVendor");
+    let addPriceelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addPrice");
+
+    let myModel : string = addModelelement.value;
+    let myVendor: string = addVendorelement.value;
+    let myPrice : number = +addPriceelement.value;  
+
+    axios.post<ICar>("https://webapicar20190326034339.azurewebsites.net/api/cars",
+                    {model:myModel,vendor:myVendor,price:myPrice})
+                    .then(function (response :  AxiosResponse): void
+                    {
+                        console.log("Statuskoden er :" + response.status);
+                    })
+                    .catch(
+                        function (error:AxiosError) : void{                          
+                            console.log(error);
+                        }
+                    )
+                    
+
+}
 
 function showAllCars():void{
 
@@ -21,7 +49,7 @@ function showAllCars():void{
         let result: string = "<ol>" 
 
         response.data.forEach((car: ICar) => {
-            result += "<li>"+ car.model + " " +car.vendor+ " "+car.id  +"</li>"
+            result += "<li>"+ car.model + " " +car.vendor+ " "+car.price  +"</li>"
         });
         result += "</ol>"
 
@@ -41,4 +69,4 @@ function showAllCars():void{
 
 
 
-showAllCars();
+// showAllCars();
